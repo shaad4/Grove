bd03f0e100fb85d88b1c9254e8bc9a3659be0810
shaad4 <mohammedshaadn@gmail.com>
2026-05-15 10:40:37 +0530

    feat(tenants): implement tenant management with middleware and models

--- backend/apps/tenants/models.py
+++ backend/apps/tenants/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    logo = models.ImageField(upload_to="tenant_logos/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        db_table = "tenants"
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "apps.tenants.middleware.TenantMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
